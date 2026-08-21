75db661780838a4c7fcbcba2225aab696270d10e
maxwillianst <willianredeon@gmail.com>
2026-08-21 17:06:20 -0300

    Add Module 2 exercises

---
+++ Module 2/ex048.py
@@ -0,0 +1,7 @@
+('======= Exercício aula 13 =======')
+print('======= SOMA ÍMPARES MÚTIPLOS DE 3 =======')
+
+soma = 0
+for c in range(1, 501, 2):
+    total = c + c
+    print (f'{total} é a soma total')
---
+++ Module 2/ex050.py
@@ -0,0 +1,20 @@
+('======= Exercício aula 13 =======')
+print('======= SOMA DE PARES =======')
+
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+n3 = int(input('Terceiro valor: '))
+n4 = int(input('Quarto valor: '))
+n5 = int(input('Quinto valor: '))
+n6 = int(input('Sexto valor: '))
+
+if n1 % 2 == 0:
+    elif n2 % 2 == 0:
+    elif n3 % 2 == 0:
+    elif n4 % 2 == 0:
+    elif n5 % 2 == 0:
+    elif n6 % 2 == 0:
+    soma = n1 + n2 + n3 + n4 + n5 + n6
+        else:
+ print('Não tem nenhum valor par')
+print(soma)
